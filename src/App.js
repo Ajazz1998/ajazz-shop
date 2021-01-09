a2af0f4f5a77c90d141b78dd860f56ee0dfb871b
Ajay Bava <ajaybava1998@gmail.com>
2021-01-09 19:22:22 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Home from './componets/Home/home';
+import Contact from './componets/Contact/contact';
+import About from './componets/About/about';
 // import SinglePage from './componets/Singlepage/singlepage';
 import { ShopState } from "./Context/ShopContext";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
     <ShopState>
 
        <Home />
+       <Contact />
+       <About />
 
     </ShopState>
     </div>
