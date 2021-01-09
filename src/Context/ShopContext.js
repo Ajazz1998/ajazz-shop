1e5ea8afe4e98ed766282d9f415e39f499b258df
Ajay Bava <ajaybava1998@gmail.com>
2021-01-09 14:38:47 +0000

    commit

---
+++ src/Context/ShopContext.js
@@ -0,0 +1,33 @@
+import React, { useState, createContext, useEffect } from "react";
+
+export const ShopContext = createContext();
+
+export const ShopState = ({ children }) => {
+    const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const getProducts = async () => {
+        const response = await fetch(
+          `https://api-movie-my.herokuapp.com/movies`
+        );
+        const data = await response.json();
+        setProducts(data);
+      };
+
+      useEffect(() => {
+        getProducts();
+      }, []);
+
+    return (
+        <ShopContext.Provider
+        value={{
+            products,
+            setProducts,
+            isLoading,
+            setIsLoading,
+        }}
+        >
+        {children}
+        </ShopContext.Provider>
+    )
+}
