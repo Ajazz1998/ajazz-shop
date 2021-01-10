d8778c67a3d39065d58cb19a35520ffd83b184df
Ajay Bava <ajaybava1998@gmail.com>
2021-01-10 15:14:06 +0000

    commit

--- src/componets/Navbar/Mainnav/mainnav.js
+++ src/componets/Navbar/Mainnav/mainnav.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 // import { faBars } from "@fortawesome/free-solid-svg-icons";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import './mainnav.css';
+import '../mainnav.css';
 
 class MainNav extends Component {
   constructor () {
