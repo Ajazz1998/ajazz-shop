9d45768c11086c9d3c242dfaf19bf02f249a6cda
Ajay Bava <ajaybava1998@gmail.com>
2021-01-14 14:38:54 +0000

    commit

--- src/componets/Products/products.js
+++ src/componets/Products/products.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { ShopContext } from "../../Context/ShopContext";
 import Container from "../ReusableComponets/Container";
 import Loader from "../ReusableComponets/loader";
+import './p_style.css';
 
 const Products = () => {
   const { products, isLoading } = useContext(ShopContext);
@@ -9,22 +10,25 @@ const Products = () => {
   return (
     <div className="products">
       <Container>
-        {products && products.length === 0 && (
-          <h1 className="error">Result not found</h1>
-        )}
-        {!isLoading ? (
-          products &&
-          products.map((Products) => (
-            <img
-              key={Products.id}
-              src={`${Products.img}`}
-              alt="poster"
-              className="img"
-            />
-          ))
-        ) : (
-          <Loader />
-        )}
+        <div className="product">
+            {products && products.length === 0 && (
+              <h1 className="error">Result not found</h1>
+            )}
+            {!isLoading ? (
+              products &&
+              products.map((Products) => (
+                <img
+                  key={Products.id}
+                  src={`${Products.img}`}
+                  alt="poster"
+                  className="img"
+                />
+              ))
+            ) : (
+              <Loader />
+            )}
+        </div>
+        
       </Container>
     </div>
   );
