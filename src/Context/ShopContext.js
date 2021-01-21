ea21971e0655080768e0e7606e0f0d6f865284ca
Ajay Bava <ajaybava1998@gmail.com>
2021-01-21 20:17:26 +0000

    commit

--- src/Context/ShopContext.js
+++ src/Context/ShopContext.js
@@ -5,19 +5,28 @@ export const ShopContext = createContext();
 export const ShopState = ({ children }) => {
     const [products, setProducts] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage] = useState(9);
 
     const getProducts = async () => {
+      setIsLoading(true);
         const response = await fetch(
           `https://api-movie-my.herokuapp.com/movies`
         );
         const data = await response.json();
         console.log(data)
         setProducts(data);
+        setIsLoading(false);
       };
 
       useEffect(() => {
         getProducts();
       }, []);
+
+        // Get current posts
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = products.slice(indexOfFirstPost, indexOfLastPost);
 
     return (
         <ShopContext.Provider
@@ -26,6 +35,10 @@ export const ShopState = ({ children }) => {
             setProducts,
             isLoading,
             setIsLoading,
+            currentPage, 
+            setCurrentPage,
+            postsPerPage,
+            currentPosts
         }}
         >
         {children}
--- src/componets/Products/products.js
+++ src/componets/Products/products.js
@@ -6,30 +6,35 @@ import Loader from "../ReusableComponets/loader";
 import './p_style.css';
 
 const Products = () => {
-  const { products, isLoading } = useContext(ShopContext);
+  const {  isLoading, currentPosts } = useContext(ShopContext);
 
   return (
     <div className="products">
     
       <Container>
         <div className="product">
-            {products && products.length === 0 && (
+            {currentPosts && currentPosts.length === 0 && (
               <h1 className="error">Result not found</h1>
             )}
             
             {!isLoading ? (
-              products &&
-              products.map((Products) => (
+              currentPosts &&
+              currentPosts.map((Products) => (
+                
                 <div className="p-box">
-                    <img
-                    key={Products.id}
-                    src={`${Products.img}`}
-                    alt="poster"
-                    className="img"
-                  />
-                <p>{Products.price}</p>
-                  <a href={Products.link}><p>{Products.title}</p></a>
-                  <p>{Products.rate}</p>
+                   <a href={Products.link}><p>{Products.title}</p></a>
+                        <img
+                        key={Products.id}
+                        src={`${Products.img}`}
+                        alt="poster"
+                        className="img"
+                      />
+                  <div className="p-details">
+                      <p className="price">{Products.price}</p>
+                      
+                      <p>{Products.rate}</p>
+                  </div>
+
                 </div>
 
               ))
