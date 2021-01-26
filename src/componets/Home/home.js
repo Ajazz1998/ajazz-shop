a4af48e32caf2cc7a3dc3f3c4c8adbc0eac95cb8
Ajay Bava <ajaybava1998@gmail.com>
2021-01-26 14:35:18 +0000

    comit

--- src/componets/Home/home.js
+++ src/componets/Home/home.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Products from '../Products/products';
 import Carousel from '../ReusableComponets/carousel';
 
--- src/componets/ReusableComponets/carousel.js
+++ src/componets/ReusableComponets/carousel.js
@@ -1,3 +1,4 @@
+
 import { useContext } from "react";
 import { ShopContext } from "../../Context/ShopContext";
 import Container from "./ReusableComponets/Container";
