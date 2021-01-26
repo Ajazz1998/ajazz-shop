3e6ad1189cc57dbcc667b0cb41da61e2ba897a34
Ajay Bava <ajaybava1998@gmail.com>
2021-01-26 14:35:45 +0000

    commit

--- src/componets/Home/home.js
+++ src/componets/Home/home.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Products from '../Products/products';
-import Carousel from '../ReusableComponets/carousel';
 
 const home = () => {
     return (
         <div>
         <Products />
-        <Carousel />
         </div>
         
         
